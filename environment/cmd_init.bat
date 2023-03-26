d937d6b2044b99d5c10c04bccad423a58bae6fbf
CloudCrusader <markuswagner93@gmail.com>
2023-03-26 09:42:00 +0200

    added script for cmder

---
+++ environment/cmd_init.bat
@@ -0,0 +1,7 @@
+@echo off
+setlocal enabledelayedexpansion
+set CMDER_ROOT=C:\tools\cmder
+set CMDER_START=C:\
+
+"C:\\tools\\cmder\\vendor\\init.bat"
+cmd.exe /k \"C:\\tools\cmder\\vendor\\cmd_init.bat\
---
+++ environment/setup.win.bat
@@ -0,0 +1,4 @@
+@echo off
+
+set TARGET_FOLDER="%CMDER_ROOT%\\vendor\\"
+copy "./cmd_init.bat" "C:\\tools\cmder\\vendor\\cmd_init.bat"
