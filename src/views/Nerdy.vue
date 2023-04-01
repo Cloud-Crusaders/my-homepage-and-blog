65692ec0c4a7b399ec7c391e66d9cb3b3e2d889e
CloudCrusader <markuswagner93@gmail.com>
2023-04-01 20:00:25 +0200

    adjusted some scripts

--- src/views/Nerdy.vue
+++ src/views/Nerdy.vue
@@ -1,10 +1,19 @@
 <template>
   <v-container>
-    Nerdy
+    <v-card variant="tonal">
+      <v-card-title>Welcome to the nerdy side!</v-card-title>
+      <v-card-text>
+      More than 10 years experience in developing and more than 5 in the cloud-computing sector.<br />
+      Explore my knowledge and the stations i have been visited.
+      </v-card-text>
+      <v-card-actions>
+        <v-btn to="https://markuswagner.dev">See more!</v-btn>
+      </v-card-actions>
+    </v-card>
   </v-container>
 </template>
 
-<script>
+<script lang="ts">
 export default {
 
 }
